38df82ff7010b8d7df89b8a62ae845c9f01683f6
saideep-arikontham <saideepreddy99@gmail.com>
2024-03-30 23:23:00 -0400

    Clustering.py changed

--- src/Clustering.py
+++ src/Clustering.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[7]:
 
 
 import pandas as pd
@@ -15,7 +15,7 @@ import seaborn as sns
 from sklearn.manifold import TSNE
 from sklearn.pipeline import make_pipeline
 from sklearn.metrics import silhouette_score
-
+from kneed import KneeLocator
 
 notes = pd.read_csv('data/processed_notes.csv')
 notes
@@ -29,7 +29,7 @@ notes
 # 
 # - Will have to consider the inclusion of processed_note_length and initial_note_length later.
 
-# In[2]:
+# In[8]:
 
 
 # setting X and y for modelling
@@ -67,7 +67,7 @@ dtm_df
 # 
 # 
 
-# In[3]:
+# In[9]:
 
 
 def cluster_score_plots(min_cluster, max_cluster, inertia_values, silhouette_scores, save_name):
@@ -92,42 +92,18 @@ def cluster_score_plots(min_cluster, max_cluster, inertia_values, silhouette_sco
 
 
 
-# ## Using K-means clustering - On sparse DTM
-
-# In[4]:
+# In[10]:
 
 
 min_cluster = 3
 max_cluster = 14
 
-inertia_values = []
-silhouette_scores = []
-
-for num_clust in range(min_cluster,max_cluster+1):
-    kmeans = KMeans(n_clusters=num_clust, random_state=42, n_init='auto').fit(dtm)
-    cluster_ids, cluster_sizes = np.unique(kmeans.labels_, return_counts=True)
-    print(f"Number elements in {num_clust} clusters: {cluster_sizes}")
-    inertia_values.append(kmeans.inertia_)
-    silhouette_scores.append(silhouette_score(dtm, kmeans.labels_))
-
-
-# In[5]:
-
-
-cluster_score_plots(min_cluster, max_cluster, inertia_values, silhouette_scores, 'figs/kmeans_clustering_scores_sparse_dtm.png')
-
-
-# ### Notes:
-# 
-# - The inertia plot does not show a proper elbow here, so its not conclusive what is a good value for number of clusters.
-# 
-# - The Silhouettre score plot shows that the best number of clusters are at "number of clusters = 9"
 
 # ## Dimensionality Reduction
 
 # ### i. Using TruncatedSVD
 
-# In[6]:
+# In[11]:
 
 
 from sklearn.decomposition import TruncatedSVD
@@ -140,7 +116,7 @@ explained_variance = lsa.explained_variance_ratio_.sum()
 print(f"Explained variance of the SVD step: {explained_variance * 100:.1f}%")
 
 
-# In[7]:
+# In[12]:
 
 
 lsa_df = pd.DataFrame(data = X_lsa, columns = ['lsa1', 'lsa2'])
@@ -161,12 +137,12 @@ plt.show()
 # 
 # - Lets see how the k-means clustering goes.
 
-# In[8]:
+# In[13]:
 
 
 from sklearn.decomposition import TruncatedSVD
 
-lsa = TruncatedSVD(n_components=100)
+lsa = TruncatedSVD(n_components=1000)
 X_lsa = lsa.fit_transform(dtm)
 explained_variance = lsa.explained_variance_ratio_.sum()
 print(f"Explained variance of the SVD step: {explained_variance * 100:.1f}%")
@@ -186,10 +162,15 @@ for num_clust in range(min_cluster,max_cluster+1):
 
 cluster_score_plots(min_cluster, max_cluster, inertia_values, silhouette_scores, 'figs/lsa_kmeans_scores.png')
 
+print('Best silhouette score for TruncatedSVD 1000 components:', max(silhouette_scores))
+print(f'- Best silhouette score is observed at n_clusters = {min_cluster + silhouette_scores.index(max(silhouette_scores))}')
+
+print(f'- n_clusters account to Inertia scores are:', inertia_values.index(KneeLocator(inertia_values, list(range(min_cluster, max_cluster+1)), curve='convex', direction='decreasing').knee))
+
 
 # ### ii. Using UMAP
 
-# In[9]:
+# In[52]:
 
 
 reducer = umap.UMAP(n_neighbors = 5, n_components = 2, metric = 'euclidean', min_dist = 0.05, spread = 1.0, random_state=42)
@@ -210,7 +191,9 @@ plt.show()
 # 
 # 
 
-# In[10]:
+# #### UMAP K-means
+
+# In[53]:
 
 
 inertia_values = []
@@ -225,12 +208,17 @@ for num_clust in range(min_cluster,max_cluster+1):
 
 cluster_score_plots(min_cluster, max_cluster, inertia_values, silhouette_scores, 'figs/umap_kmeans_scores.png')
 
+print('Best silhouette score for UMAP 2 components:', max(silhouette_scores))
+print(f'- Best silhouette score is observed at n_clusters = {min_cluster + silhouette_scores.index(max(silhouette_scores))}')
+
+print(f'- n_clusters account to Inertia scores are:', inertia_values.index(KneeLocator(inertia_values, list(range(min_cluster, max_cluster+1)), curve='convex', direction='decreasing').knee))
+
 
 # - The inertia scores don't show a clear elbow to decide number of clusters.
 # 
 # - The silhouette scores indicate that the number of clusters = 10.
 
-# In[11]:
+# In[54]:
 
 
 #plotting for kmeans 10 clusters and Checking against the target- case_num
@@ -255,13 +243,101 @@ plt.savefig('figs/umap_kmeans_casenum.png')
 plt.show()
 
 
+# In[55]:
+
+
+from sklearn.metrics.cluster import adjusted_rand_score
+from sklearn.metrics.cluster import contingency_matrix
+
+mapping = {6: 0, 
+           9: 1, 
+           4: 2, 
+           1: 3, 
+           0: 4, 
+           3: 5, 
+           8: 6, 
+           5: 7, 
+           7: 8, 
+           2: 9}
+
+relabeled_labels = np.vectorize(mapping.get)(kmeans.labels_)
+print('Adjusted rand score for the Kmeans clustering solution:',adjusted_rand_score(y, relabeled_labels))
+print('Contingency matrix:')
+contingency_matrix(y, relabeled_labels)
+
+
 # - We can see that there is overlap in patient case_num from the clusters formed.
 # 
 # - Considering the context of patient notes, this overlap might be due to the similar symptoms.
 
+# #### UMAP HDBSCAN
+
+# In[56]:
+
+
+from sklearn.cluster import HDBSCAN
+import seaborn as sns
+
+hdb_tsne = HDBSCAN(min_cluster_size=500, min_samples = 100)
+cluster_labels = hdb_tsne.fit_predict(red1)
+
+plt.figure(figsize = (20,15))
+
+palette = sns.color_palette('dark', np.unique(cluster_labels).max() + 1)
+palette[7] = (0.8, 0.1, 0.1)
+palette[5] = (0.1, 0.3, 0.7)
+palette[3] = (0.5, 0.8, 0)
+palette[0] = (0, 0.8, 0.8)
+
+#plotting clusters
+for i in range(np.unique(cluster_labels).max() + 1):
+    plt.scatter(red1[cluster_labels == i]['umap_1'], red1[cluster_labels == i]['umap_2'], color = palette[i], label = f'Cluster {i+1}')
+
+#plotting noise
+plt.scatter(red1[cluster_labels == -1]['umap_1'], red1[cluster_labels == -1]['umap_2'], color = 'black', marker = 'x', label = f'Noise')
+
+
+plt.title('HDBSCAN Clustering (2D UMAP)')
+plt.xlabel('umap1')
+plt.ylabel('umap2')
+plt.xlim(-10, 30)
+plt.ylim(-10, 25)
+plt.legend(loc = 'best', fontsize = 'medium', ncols =2)
+plt.savefig('figs/hdbscan_clusters.png')
+plt.show()
+
+
+# In[65]:
+
+
+clustering_df = pd.DataFrame()
+clustering_df['y'] = y
+clustering_df['y_pred'] = cluster_labels
+no_noise = clustering_df[clustering_df['y_pred'] != -1]
+
+mapping = {6: 0, 
+           0: 1, 
+           3: 2, 
+           2: 3, 
+           7: 4, 
+           9: 5, 
+           5: 6, 
+           8: 7, 
+           1: 8, 
+           4: 9,
+          10:10}
+
+no_noise['y_pred'] = no_noise['y_pred'].replace(mapping)
+
+
+print('Adjusted rand score for the DBSCAN clustering solution:',adjusted_rand_score(no_noise['y'], no_noise['y_pred']))
+print('Contingency matrix:')
+contingency_matrix(no_noise['y'], no_noise['y_pred'])
+
+
 # ### Using T-SNE
 
-# In[12]:
+# In[66]:
 
 
 tsne = TSNE(n_components=2,  random_state = 42, init='random')
@@ -278,7 +354,9 @@ plt.savefig('figs/tsne_2components.png')
 plt.show()
 
 
-# In[13]:
+# #### T-SNE KMeans
+
+# In[67]:
 
 
 inertia_values = []
@@ -293,8 +371,13 @@ for num_clust in range(min_cluster,max_cluster+1):
 
 cluster_score_plots(min_cluster, max_cluster, inertia_values, silhouette_scores, 'figs/tsne_kmeans_scores.png')
 
+print('Best silhouette score for T-SNE 2 components:', max(silhouette_scores))
+print(f'- Best silhouette score is observed at n_clusters = {min_cluster + silhouette_scores.index(max(silhouette_scores))}')
 
-# In[14]:
+print(f'- n_clusters account to Inertia scores are:', inertia_values.index(KneeLocator(inertia_values, list(range(min_cluster, max_cluster+1)), curve='convex', direction='decreasing').knee))
+
+
+# In[68]:
 
 
 #plotting for kmeans 10 clusters and Checking against the target- case_num
@@ -318,7 +401,29 @@ plt.savefig('figs/tnse_kmeans_casenum.png')
 plt.show()
 
 
-# In[15]:
+# In[69]:
+
+
+'''mapping = {6: 0, 
+           9: 1, 
+           4: 2, 
+           1: 3, 
+           0: 4, 
+           3: 5, 
+           8: 6, 
+           5: 7, 
+           7: 8, 
+           2: 9}
+
+relabeled_labels = np.vectorize(mapping.get)(kmeans.labels_)'''
+print('Adjusted rand score for the Kmeans clustering solution:',adjusted_rand_score(y, kmeans.labels_))
+print('Contingency matrix:')
+contingency_matrix(y, kmeans.labels_)
+
+
+# #### T-SNE HDBSCAN
+
+# In[70]:
 
 
 from sklearn.cluster import HDBSCAN
@@ -351,6 +456,33 @@ plt.ylim(-80, 80)
 plt.legend(loc = 'best', fontsize = 'medium', ncols =2)
 plt.savefig('figs/hdbscan_clusters.png')
 plt.show()
+
+
+# In[72]:
+
+
+clustering_df = pd.DataFrame()
+clustering_df['y'] = y
+clustering_df['y_pred'] = cluster_labels
+no_noise = clustering_df[clustering_df['y_pred'] != -1]
+
+mapping = {4: 0, 
+           1: 1, 
+           3: 2, 
+           7: 3, 
+           9: 4, 
+           8: 5, 
+           0: 6, 
+           5: 7, 
+           2: 8, 
+           6: 9}
+
+no_noise['y_pred'] = no_noise['y_pred'].replace(mapping)
+
+
+print('Adjusted rand score for the DBSCAN clustering solution:',adjusted_rand_score(no_noise['y'], no_noise['y_pred']))
+print('Contingency matrix:')
+contingency_matrix(no_noise['y'], no_noise['y_pred'])
 
 
 # In[ ]:
